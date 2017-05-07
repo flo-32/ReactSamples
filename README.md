8643835fc8d3776d857e6bd90aff861c8be55036
XXXII <XXXII@XXXIIs-MBP.fritz.box>
2017-05-07 15:32:10 +0200

    init React Repo

---
+++ README.md
@@ -0,0 +1,6 @@
+## React Boilerplate
+1. Navigate to folder
+2. *NPM install*
+3. *NPM run dev*
+4. Navigate to localhost:8080
+5. Tutorial auf [GitHub](https://github.com/learncodeacademy/react-js-tutorials)
---
+++ src/js/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class Header extends React.Component {
+  handleChange(e) {
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
